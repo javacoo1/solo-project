b79d8fca136e2639e8cf69b22065c48be1467a49
JinhyunLee <leejinhyun99@naver.com>
2025-01-26 04:04:04 +0900

    [create] Create index.html, HomeController and home.html

---
+++ src/main/java/soloProject/demo/HomeController.java
@@ -0,0 +1,16 @@
+package soloProject.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("home")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
